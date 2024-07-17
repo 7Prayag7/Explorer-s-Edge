ead0b84a2acb170060825dc9d81deaf0d68fa150
Prayag Rastogi <rastogiprayag@gmail.com>
2024-07-17 22:46:03 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+import { Resend } from 'resend';
+const resend = new Resend('re_NWMx3czy_5fm2q1hrJ1bKkgFh5no1fdyD');
 
 module.exports.renderRegister = (req, res) => {
     res.render('users/register');
@@ -9,6 +11,18 @@ module.exports.register = async (req, res, next) => {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
+        // Send a welcome email to the user
+        try {
+            await resend.emails.send({
+                from: 'Acme <onboarding@resend.dev>',
+                to: [`${email}`],
+                subject: 'Welcome to Our Platform',
+                html: '<strong>Welcome! It works!</strong>',
+            });
+            console.log('Welcome email sent successfully.');
+        } catch (emailError) {
+            console.error('Error sending welcome email:', emailError);
+        }
         req.login(registeredUser, err => {
             if (err) return next(err);
             req.flash('success', 'Welcome to Yelp Camp!');
